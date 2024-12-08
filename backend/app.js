3d7597ceda1d4834ff3f71e3e9cee80335bf5357
Shlok Kohli <shlokkohli11@gmail.com>
2024-12-08 18:05:43 +0530

    final changes

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import postRoutes from './routes/post.routes.js';
 import notificationRoutes from './routes/notification.routes.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
@@ -12,6 +13,15 @@ app.use(express.json({limit: "5mb"}))
 app.use(cookieParser())
 app.use(express.urlencoded( {extended: true} ))
 app.use(cors({origin: "http://localhost:5173", credentials: true}));
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 // Routes declaration
 app.use("/api/auth", authRoutes);
